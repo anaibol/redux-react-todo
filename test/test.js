4f1a3cf035055008ca77c9952060599e5a71b328
Juan Anibal Micheli <hey@anib.al>
2015-12-09 17:37:35 +0100

    Started using mocha, chai and enzyme

---
+++ test/test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+import TodoItem from '../app/components/TodoItem';
+
+describe('<TodoApp />', () => {
+
+    it('should render three <TodoItem /> components', () => {
+        const wrapper = shallow(<TodoItem />);
+        expect(wrapper.find(TodoItem)).to.have.length(3);
+    });
+
+    //it('should render an `.icon-star`', () => {
+    //    const wrapper = shallow(<MyComponent />);
+    //    expect(wrapper.find('.icon-star')).to.have.length(1);
+    //});
+    //
+    //it('should render children when passed in', () => {
+    //    const wrapper = shallow(
+    //        <MyComponent>
+    //            <div className="unique" />
+    //        </MyComponent>
+    //    );
+    //    expect(wrapper.contains(<div className="unique" />)).to.be.true;
+    //});
+    //
+    //it('simulates click events', () => {
+    //    const onButtonClick = sinon.spy();
+    //    const wrapper = shallow(
+    //        <Foo onButtonClick={onButtonClick} />
+    //    );
+    //    wrapper.find('button').simulate('click');
+    //    expect(onButtonClick.calledOnce).to.be.true;
+    //});
+
+});
