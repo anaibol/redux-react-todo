3e17a41a5b435d86309d24ad1e27db2814d76243
anaibol <hey@anib.al>
2016-02-08 14:27:41 +0100

    Cleaning

--- app/containers/Todo.js
+++ app/containers/Todo.js
@@ -11,21 +11,6 @@ import * as TodoActions from '../actions/todo'
 // import LogMonitor from 'redux-devtools-log-monitor'
 // import DockMonitor from 'redux-devtools-dock-monitor'
 
-//@connect(state => ({todos: state.todos}))
-// export default class Todo extends Component {
-//   render() {
-// 		const { todos } = this.props
-// 		const { addTodo, removeTodo } = this.props.actions
-//
-//   	return (
-// 			<div>
-// 				<List todos={todos} onRemoveTodo={removeTodo} />
-// 				<Input onAddTodo={addTodo} />
-// 			</div>
-// 	  )
-// 	}
-// }
-
 const Todo = ({todos, actions}) => (
 	<div>
 		<List todos={todos} removeTodo={actions.removeTodo} />
@@ -36,7 +21,3 @@ export default connect(
 	state => (state),
 	dispatch => ({actions: bindActionCreators(TodoActions, dispatch)})
 )(Todo)
-
-// export default connect()(Todo)
-
-//export default Todo
